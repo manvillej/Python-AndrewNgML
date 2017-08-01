8adf3ff138ecc499d87efa4cba4437fd784f5a94
manvillej <jeffmanvillejr@gmail.com>
2017-08-01 18:14:27 -0400

    Ex1 - completed WarmupExercise
    completed warmupExercise, creating a 5x5 identity matrix using numpy

---
+++ ex1.py
@@ -0,0 +1,33 @@
+## Machine Learning Online Class - Exercise 1: Linear Regression
+
+#  Instructions
+#  ------------
+#
+#  This is the initialization file for exercise 1 of Andrew Ng's Machine learning course
+#  All of the file have been converted into a python implementation instead of the original
+#  Matlab implementation. This 
+#
+#     warmUpExercise.py - complete
+#     plotData.py - incomplete
+#     gradientDescent.py - incomplete
+#     computeCost.py - incomplete
+#     gradientDescentMulti.py - incomplete
+#     computeCostMulti.py - incomplete
+#     featureNormalize.py - incomplete
+#     normalEqn.py - incomplete
+#
+# x refers to the population size in 10,000s
+# y refers to the profit in $10,000s
+#
+# 
+## ==================== Part 1: Basic Function ====================
+# Complete warmUpExercise.py
+import numpy as np
+
+print("running warmUpExercise...")
+print('5x5 Identity Matrix:')
+
+eye = np.identity(5)
+print(eye)
+
+
