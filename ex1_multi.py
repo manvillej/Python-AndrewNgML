936ac1546e57dbcb5f582cb048cab9643fe56d5e
manvillej <jeffmanvillejr@gmail.com>
2017-08-08 20:15:48 -0400

    Completed: Feature Normalize, computeCostMulti, and gradientDescentMulti
    Completed all, but normalEqn module for the Ex1_multi. Next part is the final part 3 for Ex1_multi to complete Ex1

--- ex1_multi.py
+++ ex1_multi.py
@@ -10,14 +10,14 @@
 #  You will need to complete the following functions in this 
 #  exericse:
 #
-#     warmUpExercise.m
-#     plotData.m
-#     gradientDescent.m
-#     computeCost.m
-#     gradientDescentMulti.m
-#     computeCostMulti.m
-#     featureNormalize.m
-#     normalEqn.m
+#     warmUpExercise.py - complete
+#     plotData.py - complete
+#     gradientDescent.py - complete
+#     computeCost.py - complete
+#     gradientDescentMulti.py - complete
+#     computeCostMulti.py - complete
+#     featureNormalize.py - complete
+#     normalEqn.py - incomplete
 #
 #  For this part of the exercise, you will need to change some
 #  parts of the code below for various experiments (e.g., changing
@@ -30,10 +30,88 @@
 #
 ## Clear and Close Figures
 import numpy as np
+import ex1helper as helper
+import matplotlib.pyplot as plt
+
+
 
 print('Loading data...')
 data = np.genfromtxt('ex1data2.txt', delimiter=',')
 x = np.array(data[:,:2])
 y = np.array(data[:,2])
-print(x.shape)
-print(y.shape)
+m = y.shape[0]
+
+
+# Print out some data points
+print('First 10 examples from the dataset: ')
+for i in range(0,10):
+	print("x = [%.0f %.0f], y = %.0f" % (x[i,0], x[i,1], y[i]))
+
+input('Program paused. Press enter to continue: ')
+
+print('\nNormalize Features...')
+
+x,mu,sigma = helper.featureNormalize(x)
+
+
+#add bias unit
+r = x
+x = np.ones((x.shape[0], x.shape[1]+1))
+x[:,1:] = r
+
+
+## ================ Part 2: Gradient Descent ================
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: We have provided you with the following starter
+#               code that runs gradient descent with a particular
+#               learning rate (alpha). 
+#
+#               Your task is to first make sure that your functions - 
+#               computeCost and gradientDescent already work with 
+#               this starter code and support multiple variables.
+#
+#               After that, try running gradient descent with 
+#               different values of alpha and see which one gives
+#               you the best result.
+#
+#               Finally, you should complete the code at the end
+#               to predict the price of a 1650 sq-ft, 3 br house.
+#
+# Hint: By using the 'hold on' command, you can plot multiple
+#       graphs on the same figure.
+#
+# Hint: At prediction, make sure you do the same feature normalization.
+#
+print('\n\nPart 1 complete.')
+print('\nRunning gradient descent ...');
+
+# Choose some alpha value
+alpha = 0.01
+num_iters = 400
+
+# Init Theta and Run Gradient Descent 
+theta = np.zeros(3);
+[theta, J_history] = helper.gradientDescentMulti(x, y, theta, alpha, num_iters);
+plt.plot(range(0,num_iters), J_history, color='blue', linestyle='solid')
+plt.xlabel('iterations')
+plt.ylabel('Cost J')
+plt.show()
+print('Theta computed from gradient descent:', theta)
+
+# Estimate the price of a 1650 sq-ft, 3 br house
+# ====================== YOUR CODE HERE ======================
+# Recall that the first column of X is all-ones. Thus, it does
+# not need to be normalized.
+
+
+r = np.array([1650, 3])
+r = (r - mu)/sigma
+r2 = np.ones(r.shape[0]+1)
+r2[1:] = r
+r = r2
+price = np.matmul(r,theta)
+
+# ============================================================
+print('\nPredicted price of a 1650 sq-ft, 3 br house (using gradient descent): ${0:.2f}'.format(price))
+input('\nPart 2 complete. Program paused. Press enter to continue: ')
--- ex1helper.py
+++ ex1helper.py
@@ -21,5 +21,29 @@ def gradientDescent(x, y, theta, alpha, iterations):
 
 	return [theta, costHistory]
 
+def normalize(x,mu,std):
+	return (x - mu)/std
 
-	
+
+def featureNormalize(x):
+	vfunc = np.vectorize(normalize)
+	mu = np.mean(x,axis=0)
+	sigma = np.std(x,axis=0)
+	x_norm = vfunc(x,mu,sigma)
+
+	return[x_norm, mu, sigma]
+
+def computeCostMulti(x, y, theta):
+	m=y.shape[0]
+	j = np.sum(np.power(np.matmul(x,theta)-y,2))/(2*m)
+	return j
+
+def gradientDescentMulti(x, y, theta, alpha, num_iters):
+	m = y.shape[0]
+	j_history = np.zeros(num_iters)
+	for i in range(0,num_iters):
+		error = np.matmul(x,theta)-y
+		theta = theta - alpha*np.dot(error,x)/m
+		j_history[i] = computeCostMulti(x,y,theta)
+
+	return [theta, j_history]
