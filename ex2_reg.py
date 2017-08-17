93c3c8fb70af32b6db292bb53b8e9850d12df567
manvillej <jeffmanvillejr@gmail.com>
2017-08-17 15:15:33 -0400

    added mapFeature function
    MapFeature function to ex2helper. next up costFunctionReg

---
+++ ex2_reg.py
@@ -0,0 +1,53 @@
+## Machine Learning Online Class - Exercise 2: Logistic Regression
+#
+#  Instructions
+#  ------------
+#
+#  This file contains code that helps you get started on the second part
+#  of the exercise which covers regularization with logistic regression.
+#
+#  You will need to complete the following functions in this exericse:
+#
+#     sigmoid.m
+#     costFunction.m
+#     predict.m
+#     costFunctionReg.m
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
+
+## Initialization
+
+## Load Data
+#  The first two columns contains the X values and the third column
+#  contains the label (y).
+import numpy as np
+import ex2helper as helper
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('./data/ex2data2.txt', delimiter=',')
+y = np.array(data[:,2])
+x = np.array(data[:,0:2])
+
+## =========== Part 1: Regularized Logistic Regression ============
+#  In this part, you are given a dataset with data points that are not
+#  linearly separable. However, you would still like to use logistic
+#  regression to classify the data points.
+#
+#  To do so, you introduce more features to use -- in particular, you add
+#  polynomial features to our data matrix (similar to polynomial
+#  regression).
+#
+
+# Add Polynomial Features
+
+x = helper.mapFeatures(x)
+
+# Initialize fitting parameters
+initial_theta = np.zeros([x.shape[1], 1])
+print(initial_theta.shape)
+
+
+# Set regularization parameter lambda to 1
+lambdaVal = 1
--- ex2helper.py
+++ ex2helper.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as op
 
-def plotData(X, Y):
+def plotData(X, Y, stringX, stringY):
 	#np.extract(Y==1,X[0]) returns an array of values in X where the value is 1 in the same location in Y
-	positiveExamples = plt.scatter(np.extract(Y==1,X[:,0]), np.extract(Y==1,X[:,1]), label = "Admitted", marker='o', color='b', s=10)
-	negativeExamples = plt.scatter(np.extract(Y==0,X[:,0]), np.extract(Y==0,X[:,1]), label = "Not Admitted", marker='x', color='r', s=10)	
+	positiveExamples = plt.scatter(np.extract(Y==1,X[:,0]), np.extract(Y==1,X[:,1]), label = "y=1", marker='o', color='b', s=10)
+	negativeExamples = plt.scatter(np.extract(Y==0,X[:,0]), np.extract(Y==0,X[:,1]), label = "y=0", marker='x', color='r', s=10)	
 	plt.legend(handles=[positiveExamples, negativeExamples], loc='lower left')
 	plt.xlabel('Exam 1 Score')
 	plt.ylabel('Exam 2 Score')
-	plt.title('Student Applicants\' Test Scores')
 	plt.show()
+	
 
 def sigmoid(z):
 	return np.array([1/(1+np.exp(-z))]).transpose()
@@ -19,6 +20,35 @@ def costFunction(theta, x, y):
 	m = x.shape[0]
 	z = sigmoid(np.matmul(x,theta))
 	J = -1/m*(np.sum(np.log(z)*y+np.log(1-z)*(1-y)))
-	grad = np.matmul(x.transpose(),z-y)/m
+	return J
 
-	return [J, grad]
+def gradient(theta, x, y):
+	m = x.shape[0]
+	z = sigmoid(np.matmul(x,theta))
+	positiveExamples = plt.scatter(np.extract(Y==1,X[:,1]), np.extract(Y==1,X[:,2]), label = "y=1", marker='o', color='b', s=10)
+	negativeExamples = plt.scatter(np.extract(Y==0,X[:,1]), np.extract(Y==0,X[:,2]), label = "y=0", marker='x', color='r', s=10)	
+	#Only need 2 points to define a line, so choose two endpoints
+	plot_x = np.array([min(X[:, 2]),  max(X[:, 2])])
+	# Calculate the decision boundary line
+	plot_y = (-1./theta[2])*(theta[1]*plot_x + theta[0])
+	# Plot, and adjust axes for better viewing
+	plt.plot(plot_x, plot_y)
+
+	plt.legend(handles=[positiveExamples, negativeExamples], loc='lower left')
+	plt.xlabel(stringX)
+	plt.ylabel(stringY)
+	plt.show()
+
+def mapFeatures(X):
+	degrees = 6
+
+	[m,n] = X.shape
+	for i in range(1, degrees):
+		for j in range(0,i):
+			r = np.multiply(np.power(X[:,0],i-j),np.power(X[:,1],j))
+			X = np.append(X,r[:,np.newaxis],axis=1)
+
+	X = np.insert(X,0,1,axis=1)
+
+	return X
+	
