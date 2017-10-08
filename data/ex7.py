8f842749f6a639d94685561b965453872c998f99
manvillej <jeffmanvillejr@gmail.com>
2017-10-08 12:03:27 -0400

    Added data and started Ex7

---
+++ data/ex7.py
@@ -0,0 +1,27 @@
+## Machine Learning Online Class
+#  Exercise 7 | Principle Component Analysis and K-Means Clustering
+#
+#  Instructions
+#  ------------
+#
+#  This file contains code that helps you get started on the
+#  exercise. You will need to complete the following functions:
+#
+#     pca.m
+#     projectData.m
+#     recoverData.m
+#     computeCentroids.m - completed
+#     findClosestCentroids.m - completed
+#     kMeansInitCentroids.m - completed
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
+## Imports
+## ================= Part 1: Find Closest Centroids ====================
+#  To help you implement K-Means, we have divided the learning algorithm 
+#  into two functions -- findClosestCentroids and computeCentroids. In this
+#  part, you should complete the code in the findClosestCentroids function. 
+#
+
+print('Finding closest centroids.')
