8df4d9ec5d4966bc040a274448749add7aac100c
manvillej <jeffmanvillejr@gmail.com>
2017-10-14 23:02:10 -0400

    added part 1 and 2 of Ex8_cofi
    added the initial data visualization (part 1) and Collaborative Filtering Cost Function (part 2)
    
    I also added a fix to ex5helper where one variable was not completely renamed in all the references

---
+++ ex8_Cofi.py
@@ -0,0 +1,88 @@
+## Machine Learning Online Class
+#  Exercise 8 | Anomaly Detection and Collaborative Filtering
+#
+#  Instructions
+#  ------------
+#
+#  This file contains code that helps you get started on the
+#  exercise. You will need to complete the following functions:
+#
+#     estimateGaussian.m
+#     selectThreshold.m
+#     cofiCostFunc.m
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
+## imports
+import scipy.io as io
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import ex8helper as helper
+
+## =============== Part 1: Loading movie ratings dataset ================
+#  You will start by loading the movie ratings dataset to understand the
+#  structure of the data.
+#  
+print('Loading movie ratings dataset....')
+
+#load data
+mat = io.loadmat('./data/ex8_movies.mat')
+Y = mat['Y']
+R = mat['R']
+
+
+#  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies on 
+#  943 users
+#
+#  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+#  rating to movie i
+
+#  From the matrix, we can compute statistics like average rating.
+print('Average rating for movie 1 (Toy Story): {:.2f} / 5'.format(np.mean(Y[0,R[0,:]])))
+
+#  We can "visualize" the ratings matrix by plotting it with imagesc
+plt.imshow(Y, cmap='Greys', norm=colors.Normalize(vmin=Y.min(),vmax=Y.max()))
+plt.xlabel('Movies')
+plt.ylabel('Users')
+plt.show()
+
+input('\nPart 1 completed. Program paused. Press enter to continue: ')
+
+## ============ Part 2: Collaborative Filtering Cost Function ===========
+#  You will now implement the cost function for collaborative filtering.
+#  To help you debug your cost function, we have included set of weights
+#  that we trained on that. Specifically, you should complete the code in 
+#  cofiCostFunc.m to return J.
+
+#  Load pre-trained weights (X, Theta, num_users, num_movies, num_features)
+print('Testing the cost function for Collaborative Filtering...\n')
+
+mat = io.loadmat('./data/ex8_movieParams.mat')
+X = mat['X']
+Theta = mat['Theta']
+numUsers = mat['num_users']
+numMovies = mat['num_movies']
+numFeatures = mat['num_features']
+
+#  Reduce the data set size so that this runs faster
+numUsers = 4
+numMovies = 5
+numFeatures = 3
+
+X = X[0:numMovies,0:numFeatures]
+Theta = Theta[0:numUsers,0:numFeatures]
+Y = Y[0:numMovies,0:numUsers]
+R = R[0:numMovies,0:numUsers]
+
+
+#  Evaluate cost function
+params = np.append(X.flatten(), Theta.flatten())
+J = helper.cofiCostFunction(params, Y, R, numUsers, numMovies, numFeatures, 0)
+
+print('Cost at loaded parameters: {:.2f}'.format(J))
+print('(this value should be about 22.22)')
+
+
+input('\nPart 2 completed. Program paused. Press enter to continue: ')
--- ex8helper.py
+++ ex8helper.py
@@ -78,3 +78,110 @@ def selectThreshold(yval, pval):
 			bestEpsilon = epsilon
 
 	return bestF1, bestEpsilon
+
+def cofiCostFunction(params, Y, R, numUsers, numMovies, numFeatures, lambdaVal):
+	#COFICOSTFUNC Collaborative filtering cost function
+	#   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+	#   num_features, lambda) returns the cost and gradient for the
+	#   collaborative filtering problem.
+	#
+	# Unfold the U and W matrices from paramsX
+	X = np.reshape(params[0:numMovies*numFeatures],[numMovies,numFeatures])
+	Theta = np.reshape(params[numMovies*numFeatures:],[numUsers,numFeatures])
+
+	            
+	# You need to return the following values correctly
+	J = 0
+	XGrad = np.zeros(X.shape)
+	ThetaGrad = np.zeros(Theta.shape)
+
+	# ====================== YOUR CODE HERE ======================
+	# Instructions: Compute the cost function and gradient for collaborative
+	#               filtering. Concretely, you should first implement the cost
+	#               function (without regularization) and make sure it is
+	#               matches our costs. After that, you should implement the 
+	#               gradient and use the checkCostFunction routine to check
+	#               that the gradient is correct. Finally, you should implement
+	#               regularization.
+	#
+	# Notes: X - num_movies  x num_features matrix of movie features
+	#        Theta - num_users  x num_features matrix of user features
+	#        Y - num_movies x num_users matrix of user ratings of movies
+	#        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+	#            i-th movie was rated by the j-th user
+	#
+	# You should set the following variables correctly:
+	#
+	#        X_grad - num_movies x num_features matrix, containing the 
+	#                 partial derivatives w.r.t. to each element of X
+	#        Theta_grad - num_users x num_features matrix, containing the 
+	#                     partial derivatives w.r.t. to each element of Theta
+	#
+	# returns the cost without regularization
+	# added a placeholder matrix to store values
+	# of the difference between predictions & actual Y values
+	# where there are actual ratings. This is to reduce
+	# the computations since it is computed frequently
+
+
+	placeholder = np.multiply(X.dot(Theta.T) - Y, R)
+
+	# Calculate Cost
+	J = np.sum(np.square(placeholder))/2 + lambdaVal/2*(np.sum(np.square(X)) + np.sum(np.square(Theta)))
+
+	return J
+
+def cofiGradFunction(params, Y, R, numUsers, numMovies, numFeatures, lambdaVal):
+	#COFICOSTFUNC Collaborative filtering cost function
+	#   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+	#   num_features, lambda) returns the cost and gradient for the
+	#   collaborative filtering problem.
+	#
+	# Unfold the U and W matrices from paramsX
+	X = np.reshape(params[0:numMovies*numFeatures],[numMovies,numFeatures])
+	Theta = np.reshape(params[numMovies*numFeatures:],[numUsers,numFeatures])
+
+	            
+	# You need to return the following values correctly
+	J = 0
+	XGrad = np.zeros(X.shape)
+	ThetaGrad = np.zeros(Theta.shape)
+
+	# ====================== YOUR CODE HERE ======================
+	# Instructions: Compute the cost function and gradient for collaborative
+	#               filtering. Concretely, you should first implement the cost
+	#               function (without regularization) and make sure it is
+	#               matches our costs. After that, you should implement the 
+	#               gradient and use the checkCostFunction routine to check
+	#               that the gradient is correct. Finally, you should implement
+	#               regularization.
+	#
+	# Notes: X - num_movies  x num_features matrix of movie features
+	#        Theta - num_users  x num_features matrix of user features
+	#        Y - num_movies x num_users matrix of user ratings of movies
+	#        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+	#            i-th movie was rated by the j-th user
+	#
+	# You should set the following variables correctly:
+	#
+	#        X_grad - num_movies x num_features matrix, containing the 
+	#                 partial derivatives w.r.t. to each element of X
+	#        Theta_grad - num_users x num_features matrix, containing the 
+	#                     partial derivatives w.r.t. to each element of Theta
+	#
+	# returns the cost without regularization
+	# added a placeholder matrix to store values
+	# of the difference between predictions & actual Y values
+	# where there are actual ratings. This is to reduce
+	# the computations since it is computed frequently
+
+
+	placeholder = np.multiply(X.dot(Theta.T) - Y, R)
+
+	# Calculate X & Theta gradients
+	XGrad = placeholder.dot(Theta) + lambdaVal*X
+	ThetaGrad = placeholder.T.dot(X) + lambdaVal*Theta
+
+	grad = np.append(XGrad.flatten(), ThetaGrad.flatten())
+
+	return grad
