e83ed1569fb43e30355d18227bac4444b3d9d022
manvillej <jeffmanvillejr@gmail.com>
2017-10-30 16:42:31 -0400

    Update Ex1helper to PEP8
    ex1helper completely up to PEP8
    
    Need docstrings for ex1helper

--- ex1helper.py
+++ ex1helper.py
@@ -1,55 +1,59 @@
 import numpy as np
 from numpy.linalg import inv
 
-def computeCost(x,y,theta):
-	
-	j = 1/2*np.mean(np.power(np.matmul(x.transpose(),theta)-y,2))
 
-	return j
+def computeCost(x, y, theta):
+
+    j = 1/2*np.mean(np.power(np.matmul(x.transpose(), theta)-y, 2))
+
+    return j
 
 
 def gradientDescent(x, y, theta, alpha, iterations):
-	m = x.shape[1]
-	costHistory = np.zeros(iterations)
-	for i in range(iterations):
+    m = x.shape[1]
+    costHistory = np.zeros(iterations)
+    for i in range(iterations):
 
-		error = np.matmul(x.transpose(),theta)-y
-		gradient = np.dot(x,error)
-		
-		theta = theta - alpha*gradient/m
-		costHistory[i]= computeCost(x,y,theta)
+        error = np.matmul(x.transpose(), theta)-y
+        gradient = np.dot(x, error)
 
-	return [theta, costHistory]
+        theta = theta - alpha*gradient/m
+        costHistory[i] = computeCost(x, y, theta)
 
-def normalize(x,mu,std):
-	return (x - mu)/std
+    return [theta, costHistory]
+
+
+def normalize(x, mu, std):
+    return (x - mu)/std
 
 
 def featureNormalize(x):
-	vfunc = np.vectorize(normalize)
-	mu = np.mean(x,axis=0)
-	sigma = np.std(x,axis=0)
-	x_norm = vfunc(x,mu,sigma)
+    vfunc = np.vectorize(normalize)
+    mu = np.mean(x, axis=0)
+    sigma = np.std(x, axis=0)
+    x_norm = vfunc(x, mu, sigma)
 
-	return[x_norm, mu, sigma]
+    return[x_norm, mu, sigma]
+
 
 def computeCostMulti(x, y, theta):
-	m=y.shape[0]
-	j = np.sum(np.power(np.matmul(x,theta)-y,2))/(2*m)
-	return j
+    m = y.shape[0]
+    j = np.sum(np.power(np.matmul(x, theta)-y, 2))/(2*m)
+    return j
+
 
 def gradientDescentMulti(x, y, theta, alpha, num_iters):
-	m = y.shape[0]
-	j_history = np.zeros(num_iters)
-	for i in range(0,num_iters):
-		error = np.matmul(x,theta)-y
-		theta = theta - alpha*np.dot(error,x)/m
-		j_history[i] = computeCostMulti(x,y,theta)
-		
-	return [theta, j_history]
+    m = y.shape[0]
+    j_history = np.zeros(num_iters)
+    for i in range(0, num_iters):
+        error = np.matmul(x, theta)-y
+        theta = theta - alpha*np.dot(error, x)/m
+        j_history[i] = computeCostMulti(x, y, theta)
+
+    return [theta, j_history]
 
 
-def normalEqn(X,Y):
-	X= np.matmul(inv(np.matmul(X.transpose(),X)),X.transpose())
-	theta = np.matmul(X,Y)
-	return theta
+def normalEqn(X, Y):
+    X = np.matmul(inv(np.matmul(X.transpose(), X)), X.transpose())
+    theta = np.matmul(X, Y)
+    return theta
